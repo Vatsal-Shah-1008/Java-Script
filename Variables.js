254633d04c501d390d9aa4623208a2fe0ef61473
Vatsal-Shah-1008 <shahvatsal664@gmail.com>
2026-06-26 09:05:20 +0530

    Variables Updated

--- Variables.js
+++ Variables.js
@@ -1,9 +1,10 @@
-let x = 10;
-let y = 20;
-let z = x + y;
-console.log("Addition is : ",z);
-
-const a = 50;
-const b = 40;
-const c = a - b;
-console.log("Substraction is : ",c);
+const AccountNumber = "010101010132"; //If you want to change the account number, you can do so by modifying this variable.
+let AccountEmail = "admin123@gmail.com"; //If you want to change the account email, you can do so by modifying this variable.
+let AccountPassword = "admin123"; //If you want to change the account password, you can do so by modifying this variable.
+let AccountBalance = 1000; //If you want to change the account balance, you can do so by modifying this variable.
+let AccountHolderName = "Vatsal Shah"; //If you want to change the account holder name, you can do so by modifying this variable.
+let AccountHolderAddress = "Ahmedabad, Gujarat, India"; //If you want to change the account holder address, you can do so by modifying this variable.
+let AccountHolderPhoneNumber = 9876543210; //If you want to change the account holder phone number, you can do so by modifying this variable.
+let AccountHolderDOB = "10/08/2004";
+let AccountNominee; // This variable is declared but not initialized, so it will be undefined.
+console.table([AccountNumber, AccountEmail, AccountPassword, AccountBalance, AccountHolderName, AccountHolderAddress, AccountHolderPhoneNumber, AccountHolderDOB, AccountNominee]);
