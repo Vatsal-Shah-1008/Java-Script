a80906500dbd1a25afce79a3fcc7dea6412fa1a8
Vatsal-Shah-1008 <shahvatsal664@gmail.com>
2026-06-29 20:24:09 +0530

    Data Types

---
+++ Data-Types.js
@@ -0,0 +1,14 @@
+// Data Types in JavaScript (Primityve Data Types)
+let Name = "Vatsal"; //String
+let Age = 21; //Integer
+let isStudent = true; //Boolean
+let Address; //Undefined
+let Phone = null; //Null
+console.table({ Name, Age, isStudent, Address, Phone });
+
+//Object Data Types in JavaScript
+console.log("typeosf Name: ", typeof Name);
+console.log("typeosf Age: ", typeof Age);
+console.log("typeosf isStudent: ", typeof isStudent);
+console.log("typeosf Address: ", typeof Address);
+console.log("typeosf Phone: ", typeof Phone);
